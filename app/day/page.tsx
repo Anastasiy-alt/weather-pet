5ae65b43d229f07fdf48d4069400dead9f0528cb
Anastasiya Volokitina <n060600@yandex.ru>
2026-04-26 03:00:17 +0500

    feat: страница 15 дней
    Карточка дня без адаптива

--- app/day/page.tsx
+++ app/day/page.tsx
@@ -1,21 +1,25 @@
 'use client'
-
-import { useWeatherStore } from '@/store/weather'
+import {useWeatherStore} from '@/store/weather'
 import Loader from "@/components/ui/loader";
+import DayCard from "@/components/cards/day";
+import stl from './day.module.sass'
 
 export default function DayPage() {
-    const { weather, location, loading } = useWeatherStore()
+    const {weather, location, loading} = useWeatherStore()
 
-    if (loading || !weather || !location) return <Loader />
+    if (loading || !weather || !location) return <Loader/>
+    const globalMin = Math.min(...weather.days.map(d => d.tempmin))
+    const globalMax = Math.max(...weather.days.map(d => d.tempmax))
 
     return (
         <div>
-            <h1>{location.city}</h1>
-            {weather.days.map(day => (
-                <div key={day.datetime}>
-                    {day.datetime} — {day.temp}°
-                </div>
-            ))}
+            <h1 className={stl.title}>{location.city}</h1>
+            <div className={stl.dayList}>
+                {weather.days.map(day => (
+                    <DayCard key={day.datetime} data={day} globalMin={globalMin} globalMax={globalMax}/>
+                ))}
+            </div>
+
         </div>
     )
 }

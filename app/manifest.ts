2e214c1489b5c18d8c47221bce78cfda41e03ad9
Anastasiya Volokitina <n060600@yandex.ru>
2026-05-07 18:22:52 +0500

    fix: pwa color

--- app/manifest.ts
+++ app/manifest.ts
@@ -4,23 +4,36 @@ export default function manifest(): MetadataRoute.Manifest {
     return {
         name: 'WeatherPet',
         short_name: 'Weather',
+        description: 'Погодное приложение',
         start_url: '/',
         display: 'standalone',
-        background_color: 'var(--white)',
-        theme_color: 'var(--white)',
+        // background_color: '#fefdf8',  // ← реальный цвет вместо var()
+        // theme_color: '#4091c9',       // ← реальный цвет
         icons: [
             {
                 src: '/web-app-manifest-192x192.png',
                 sizes: '192x192',
                 type: 'image/png',
-                purpose: "maskable"
+                purpose: 'any'
+            },
+            {
+                src: '/web-app-manifest-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+                purpose: 'maskable'
             },
             {
                 src: '/web-app-manifest-512x512.png',
                 sizes: '512x512',
                 type: 'image/png',
-                purpose: "maskable"
+                purpose: 'any'
             },
-        ],
+            {
+                src: '/web-app-manifest-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+                purpose: 'maskable'
+            },
+        ]
     }
 }
